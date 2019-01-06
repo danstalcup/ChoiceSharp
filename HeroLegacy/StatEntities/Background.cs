30945508d87e0ad8a65092c67a9de71c62c25e6e
danstalcup <billyjoelntrdame@gmail.com>
2019-01-06 11:35:48 -0500

    Add backgroud

---
+++ HeroLegacy/StatEntities/Background.cs
@@ -0,0 +1,17 @@
+﻿namespace ChoiceSharp.HeroLegacy.StatEntities
+{    
+    public enum BackgroundWealthEnum
+    {
+        Impoverished, Humble, Noble
+    }
+
+    public enum BackgroundRegionEnum
+    {
+        City, TownA, TownB, TownC, Plains, Forest
+    }    
+
+    public enum BackgroundUnusualEnum
+    {
+        Orphan, UnusualScar, DistantLands, RaisedByGoblins
+    }
+}
