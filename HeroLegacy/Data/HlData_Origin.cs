8e60dd5424137df940444b198db386bb90824b21
danstalcup <billyjoelntrdame@gmail.com>
2019-01-26 12:08:35 -0500

    Add next origin scene

--- HeroLegacy/Data/HlData_Origin.cs
+++ HeroLegacy/Data/HlData_Origin.cs
@@ -14,13 +14,19 @@ namespace ChoiceSharp.HeroLegacy.Data
             new List<StoryEvent>
             {
                 Origin_1,
-                Origin_1_Town                
+                Origin_1_Town,
+                Origin_2
             };
         public static StoryEvent Origin_1 => new StoryEvent
         {
             Id = Origin_1_Id,            
-            RawText = @"<p>It's the day of your fifteenth birthday! You awake to sunbeams on your eyelids and hop up. You gaze out to the familiar sight of...</p>
-                        <p><i>Choose your home region.</i></p>",
+            RawText = @"<p><i>Three months earlier...</i></p>
+                        <p>It's the day of your fifteenth birthday! You awake to sunbeams on your eyelids and hop up. You gaze out to the familiar sight of...</p>
+                        <p><i>Choose your home region.</i></p>
+                        <p><b>Dunnriver City</b> - The large, bustling capital city of Halia. Home the nobel council chamber and governor's house. Southwest of Greenthorn.</p>
+                        <p><b>A small town</b> - One of the three small towns within a day's walking distance of Greenthorn Academy</p>
+                        <p><b>Plains and farmland</b> - Large swaths of vibrant, grassy farmlands west of Greenthorn Academy</p>
+                        <p><b>Woodlands</b> - The dense woods of Greenthorn Forest. Not far from the academy</p>",
             RawChoices = OriginBackgroundChoices,
             NextEventId = Origin_2_Id
         };
@@ -40,13 +46,13 @@ namespace ChoiceSharp.HeroLegacy.Data
             },
             new Choice
             {
-                RawText = "Vibrant plains and farmland",
+                RawText = "Plains and farmland",
                 StatChange = new StatChange
                     {StatName = HlStats.BackgroundRegion, ResultObject = BackgroundRegionEnum.Plains}
             },
             new Choice
             {
-                RawText = "Dense woodlands",
+                RawText = "Woodlands",
                 StatChange = new StatChange { StatName = HlStats.BackgroundRegion, ResultObject = BackgroundRegionEnum.Forest }
             }
         };
@@ -81,6 +87,11 @@ namespace ChoiceSharp.HeroLegacy.Data
                 }
             };
 
+        public static StoryEvent Origin_2 => new StoryEvent
+        {
+            Id = Origin_2_Id,
+            RawText = @"As you go about your morning on your fifteenth birthday, you can't help but think how much your life circumstances have impacted your "
+        };
         public const string Origin_1_Id = "origin_1";
         public const string Origin_1_Town_Id = "origin_1_town";
         public const string Origin_2_Id = "origin_2";
